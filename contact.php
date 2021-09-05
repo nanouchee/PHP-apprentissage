3608bd271de1e722f5e0bdd1ff4c77c469067e72
nadia.mokhfi <nadia.mokhfi@hotmail.fr>
2021-09-05 10:37:20 +0200

    exercice 2 done

--- contact.php
+++ contact.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page contact</title>
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
 
+</head>
+<body>
+  
+</body>
+</html>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="#">Hello world!</a>
@@ -25,6 +38,9 @@
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="/note.php">notes </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/Exercice2.php">exercice 2 </a>
       </li>
 
 
--- test.php
+++ test.php
@@ -1,26 +1,62 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
 
-    <title>page test php </title>
-  </head>
-  <body>
-    <?php include("./partial/_navBar.php"); ?>
-    <div class="container">
+  <title>page test php </title>
+</head>
+
+<body>
+  <?php include("./partial/_navBar.php"); ?>
+  <div class="container">
 
     <h1>page test php </h1>
-    <pre>
-    <?php echo "php tests"; ?>
-    </pre>
-    </div>
 
-   
-    <script src="/js/bootstrap.bundle.min.js"></script>
-   
-  </body>
+
+    <pre>
+   résultat php 
+   ===================================================
+    
+
+
+
+    <?php
+    
+    ?>
+    
+
+  
+    
+    
+    
+    
+    
+    
+    
+    ===================================================
+
+    </pre>
+
+
+
+
+
+
+
+
+
+
+  </div>
+
+
+
+
+  <script src="/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
 </html>
